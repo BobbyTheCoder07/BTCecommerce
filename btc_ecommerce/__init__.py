dfbcc86c99bd761d96d6837d9602911c44a101e8
shivanshu19072006 <shivaaggarwal@gmail.com>
2026-07-02 11:53:12 +0530

    Patch BaseContext.__copy__ to support Python 3.14+ compatibility

--- btc_ecommerce/__init__.py
+++ btc_ecommerce/__init__.py
@@ -1,1 +1,16 @@
 # btc_ecommerce package
+import copy
+from django.template.context import BaseContext
+
+def patched_copy(self):
+    # BaseContext() compiles safely as dict_ defaults to None
+    duplicate = BaseContext()
+    duplicate.__class__ = self.__class__
+    # Manually copy instance dictionary attributes
+    duplicate.__dict__.update(self.__dict__)
+    # Perform a shallow copy of the dicts stack list
+    duplicate.dicts = self.dicts[:]
+    return duplicate
+
+# Apply the patch to support Python 3.14+ compatibility
+BaseContext.__copy__ = patched_copy
